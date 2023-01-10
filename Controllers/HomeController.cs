d728b597629748d9c7fc4bb953c326d639377049
hiyu12 <107801575+hiyu12@users.noreply.github.com>
2023-01-10 13:28:53 +0530

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -20,12 +20,12 @@ public class HomeController : Controller
 
     public IActionResult Privacy()
     {
-        return View();
+        
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        return View(new ErrorViewModel { RequestId = HttpContext.TraceIdentifier });
     }
 }
