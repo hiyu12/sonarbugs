1ff206dd5207843af88ec9ae74e2bfcdec28e3ed
hiyu12 <107801575+hiyu12@users.noreply.github.com>
2023-01-10 13:27:55 +0530

    Update ErrorViewModel.cs

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace newapp.Models;
 
 public class ErrorViewModel
 {
-    public string? RequestId { get; set; }
+    public string? RequestId {  }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
